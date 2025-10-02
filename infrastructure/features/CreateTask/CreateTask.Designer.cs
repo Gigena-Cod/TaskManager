461cc9e00d1166c8754a83b7e37b4594e22b1087
Christian Gigena <nahuel.gigena@hotmail.com>
2025-10-02 12:55:04 -0300

    Update CreateTask.Designer.cs

--- infrastructure/features/CreateTask/CreateTask.Designer.cs
+++ infrastructure/features/CreateTask/CreateTask.Designer.cs
@@ -41,53 +41,53 @@
             // 
             label1.Location = new Point(21, 20);
             label1.Name = "label1";
-            label1.Size = new Size(529, 40);
+            label1.Size = new Size(485, 40);
             label1.TabIndex = 11;
-            label1.Text = "Aplicación para registrar tareas con su descripción, fecha y estado. Permite visualizar todas las tareas en una grilla y filtrarlas por fecha o estado";
+            label1.Text = "Este formulario permite registrar nuevas tareas con su descripción, fecha de vencimiento y estado. Las tareas se mostrarán en la lista general para su control y seguimiento";
             // 
             // label4
             // 
-            label4.Location = new Point(21, 75);
+            label4.Location = new Point(21, 76);
             label4.Name = "label4";
-            label4.Size = new Size(53, 21);
+            label4.Size = new Size(96, 21);
             label4.TabIndex = 8;
-            label4.Text = "Desde:";
+            label4.Text = "Fecha limite:";
             // 
             // textBoxDescription
             // 
-            textBoxDescription.Location = new Point(176, 99);
+            textBoxDescription.Location = new Point(113, 114);
             textBoxDescription.Name = "textBoxDescription";
-            textBoxDescription.Size = new Size(374, 23);
+            textBoxDescription.Size = new Size(393, 23);
             textBoxDescription.TabIndex = 9;
             // 
             // dateTimePickerDate
             // 
-            dateTimePickerDate.Location = new Point(21, 99);
+            dateTimePickerDate.Location = new Point(113, 70);
             dateTimePickerDate.Name = "dateTimePickerDate";
             dateTimePickerDate.Size = new Size(139, 23);
             dateTimePickerDate.TabIndex = 7;
             // 
             // label5
             // 
-            label5.Location = new Point(176, 75);
+            label5.Location = new Point(20, 117);
             label5.Name = "label5";
             label5.Size = new Size(94, 21);
             label5.TabIndex = 10;
-            label5.Text = "Descripcion";
+            label5.Text = "Descripcion:";
             // 
             // button1
             // 
-            button1.Location = new Point(293, 155);
+            button1.Location = new Point(291, 178);
             button1.Name = "button1";
             button1.Size = new Size(112, 32);
             button1.TabIndex = 12;
-            button1.Text = "Crear";
+            button1.Text = "Guardar";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(157, 155);
+            button2.Location = new Point(161, 178);
             button2.Name = "button2";
             button2.Size = new Size(112, 32);
             button2.TabIndex = 13;
@@ -99,7 +99,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(563, 199);
+            ClientSize = new Size(523, 222);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label5);
@@ -108,7 +108,7 @@
             Controls.Add(textBoxDescription);
             Controls.Add(label4);
             Name = "CreateTask";
-            Text = "Crear tarea";
+            Text = "Nueva tarea";
             Load += CreateTask_Load;
             ResumeLayout(false);
             PerformLayout();
